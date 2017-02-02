d95542f10d8714d9cc3f7cf13d95e0f9ef490090
Justin Harwood <harwoodjp@g.cofc.edu>
2017-02-02 15:30:40 -0500

    navigate with #

--- static/js/app.js
+++ static/js/app.js
@@ -6,8 +6,16 @@ const aboutBtn = document.querySelector(".fa-id-card-o"),
 aboutBtn.addEventListener("click", function() {
 	contentArea.classList.add("hidden");
 	aboutBox.classList.remove("hidden");
+	document.location.hash = "#about";	
 })
 blogBtn.addEventListener("click", function() {
 	contentArea.classList.remove("hidden");
 	aboutBox.classList.add("hidden");
+	document.location.hash = "";		
 })
+
+if (document.location.hash == "#about") {
+	console.log("hash = about");
+	contentArea.classList.add("hidden");
+	aboutBox.classList.remove("hidden");
+}
